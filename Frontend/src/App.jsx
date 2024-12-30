257b265b56d8339824ffcaf01bb095c71ed23d4f
shaik sohail <shaik16sohail@gmail.com>
2024-12-30 22:19:19 +0530

    some changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import HomePage from './components/HomePage';
+// import Header from './components/Header/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </>
   )
 }
---
+++ Frontend/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/HomePage.css';
+export default function HomePage(){
+    return(
+        <div className='homepage'>
+            <h1>HomePage</h1>
+        </div>
+    );
+}
