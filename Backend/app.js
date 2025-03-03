608f0923c06d64ef18a95079df0bf800745763be
shaik sohail <shaik16sohail@gmail.com>
2025-03-03 21:37:53 +0530

    feat:integrated with database

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,22 @@
+require("dotenv").config();
 const express=require('express');
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app=express();
+const port=process.env.PORT|| 8080;
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+}
+main()
+    .then(() => {
+         console.log("Connected to MongoDB Atlas");
+     })
+    .catch((err) => console.log(err));
+
+
+
+app.listen(port,()=>{
+    console.log("server is running");
+})
