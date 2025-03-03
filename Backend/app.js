6363c90c4b7fee4e5d22a155cedcd31895b2fb57
shaik sohail <shaik16sohail@gmail.com>
2025-03-03 17:08:28 +0530

    fix:modified some packages

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,5 @@
-const express=require('express');g
+const express=require('express');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app=express();
