dbda30323f280aff1c5146d6a809ee1050d2df06
shaik sohail <shaik16sohail@gmail.com>
2025-05-18 22:09:59 +0530

    feat:Updated header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,25 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react'
 import '../index.css'
-import { Link } from 'react-router-dom';
+import axios from 'axios'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
 
-function Header ({ toggleTheme, darkMode }) {
+function Header () {
+  const { isLoggedIn, user, logout } = useAuth()
+  const navigate = useNavigate()
   const [activeLink, setActiveLink] = useState('') // Track the active link
   const handleLinkClick = link => {
     setActiveLink(link) // Set the clicked link as active
   }
-  const [typeUser,setTypeUser]=useState(false);
+  const [typeUser, setTypeUser] = useState(false)
   const [isBoxVisible, setIsBoxVisible] = useState(false)
   const popupRef = useRef(null)
   const handleDivClick = event => {
     event.stopPropagation()
     setIsBoxVisible(!isBoxVisible) // Toggle visibility
   }
-  const [isDivVisible, setIsDivVisible] = useState(false); // State to toggle the new div
+  const [isDivVisible, setIsDivVisible] = useState(false) // State to toggle the new div
 
   // Toggle the div visibility
   const toggleDiv = () => {
-    setIsDivVisible((prevState) => !prevState);
-  };
+    setIsDivVisible(prevState => !prevState)
+  }
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        'http://localhost:8080/api/auth/logout',
+        {},
+        {
+          withCredentials: true
+        }
+      )
+      logout()
+      navigate('/login')
+    } catch (err) {
+      console.log('logout failed', err)
+    }
+  }
 
   useEffect(() => {
     const handleClickOutside = event => {
@@ -36,222 +55,195 @@ function Header ({ toggleTheme, darkMode }) {
 
   return (
     <>
-    <div className='overall-head'> 
-      <div className='header container mx-auto '>
-        {/* <div className='m'> */}
-        <div className='header-logo'>
-        <div className="bars" onClick={toggleDiv}>
-        {/* <i class="fa-regular fa-user"></i> */}
-            <i className="fa-solid fa-bars text-white" style={{ cursor: "pointer" }}></i>
-          </div>
-          <a href='#'><img
-            src='/rguktLogo.png'
-            style={{
-              width: '60px',
-              height: '60px'
-            }}
-            className="rotating-image"
-          ></img></a>
-          
-          <h1 style={{color:"rgb(137, 24, 26)"}}>Stay Master</h1>
-        </div>
-        {/* New Div */}
-      {isDivVisible && (
-        <div
-          style={{
-            border:"1px solid white",
-            position: "fixed",
-            // bottom: "0",
-            top:"5rem",
-            left: "0",
-            width: "100%",
-            height: "8rem",
-            backgroundColor: "white", // Black background
-            zIndex: 2000, // Higher than other elements
-          }}
-        >
-         
-        </div>
-      )}
-{/* {typeUser==true?
-          <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/request-outpass">Request a Outpass</Link>
-          </li>
-          <li>
-            <Link to="/raise-complaint">Raise a Complaint</Link>
-          </li>
-        </ul>
-        
-        
-        :
-        <ul>
-        <li>
-          <a
-            href='#'
-            onClick={() => handleLinkClick('home')}
-            className={activeLink === 'home' ? 'active-link' : ''}
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href='#'
-            onClick={() => handleLinkClick('request-outpass')}
-            className={
-              activeLink === 'request-outpass' ? 'active-link' : ''
-            }
-          >
-            Request a Outpass
-          </a>
-        </li>
-        <li>
-          <a
-            href='#'
-            onClick={() => handleLinkClick('raise-complaint')}
-            className={
-              activeLink === 'raise-complaint' ? 'active-link' : ''
-            }
-          >
-            Raise a Complaint
-          </a>
-        </li>
-      </ul>} */}
-        <div className='header-links'>
-          <div className='first'>
-          {typeUser?
-          <ul>
-          <li>
-            <a
-              href='/caretaker/home'
-              onClick={() => handleLinkClick('home')}
-              className={activeLink === 'home' ? 'active-link' : ''}
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href='/caretaker/outpasses'
-              onClick={() => handleLinkClick('request-outpass')}
-              className={
-                activeLink === 'request-outpass' ? 'active-link' : ''
-              }
-            >
-              Outpasses
-            </a>
-          </li>
-          <li>
-            <a
-              href='/caretaker/issues'
-              onClick={() => handleLinkClick('raise-complaint')}
-              className={
-                activeLink === 'raise-complaint' ? 'active-link' : ''
-              }
-            >
-              Issues
-            </a>
-          </li>
-        </ul>:
-          <ul>
-          <li>
-            <a
-              href='#'
-              onClick={() => handleLinkClick('home')}
-              className={activeLink === 'home' ? 'active-link' : ''}
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href='#'
-              onClick={() => handleLinkClick('request-outpass')}
-              className={
-                activeLink === 'request-outpass' ? 'active-link' : ''
-              }
-            >
-              Request a Outpass
-            </a>
-          </li>
-          <li>
-            <a
-              href='#'
-              onClick={() => handleLinkClick('raise-complaint')}
-              className={
-                activeLink === 'raise-complaint' ? 'active-link' : ''
-              }
-            >
-              Raise a Complaint
-            </a>
-          </li>
-        </ul>
-          
-          }
-          </div>
-          
-          <div className='profile' onClick={handleDivClick}>
-            <img
-              src='/virat kohli.jpg'
-              alt='Profile Icon'
-              style={{ height: '1.8rem', cursor: 'pointer',height:'36px',width:'36px',borderRadius:'50%' }}
-            />
-          </div>
-          {isBoxVisible && (
-            <div
-              ref={popupRef} // Attach the reference to the popup
-              style={{
-                position: 'fixed',
-                top: '80px',
-                right: '30px',
-                padding: '2rem',
-                borderRadius: '20px',
-                color:"white",
-                width: '12.5rem',
-                height:"17rem",
-                backgroundColor: '#3B3B3B',
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-                textAlign:"center",
-                zIndex: 1000 // Ensures the dialog is above other elements
-              }}
-            >
-               <img
-              src='/virat kohli.jpg'
-              alt='Profile Icon'
-              style={{ height: '1.8rem', cursor: 'pointer',height:'72px',width:'72px',borderRadius:'50%',marginLeft:"2rem",marginBottom:'1rem' }}
-            />
-            
-
-              <h1 style={{fontSize:"1.8rem"}}> Hi Shaik !</h1>
-              <p style={{ fontSize: '15px' }}>rr200088@rguktrkv.ac.in</p>
-              <br></br>
-              <p>Profile Details</p>
-              <Link to="/" >
-                  <button
-                    onClick={() => setIsBoxVisible(false)}
-                    style={{
-                      backgroundColor: 'rgb(137, 24, 26)',
-                      color: 'white',
-                      border: 'none',
-                      padding: '5px 10px',
-                      borderRadius: '5px',
-                      cursor: 'pointer',
-                      width: '100%'
-                    }}
-                  >
-                    Logout
-                  </button>
-              </Link>
-             
+      <div className='overall-head'>
+        <div className='header container mx-auto '>
+          {/* <div className='m'> */}
+          <div className='header-logo'>
+            <div className='bars' onClick={toggleDiv}>
+              {/* <i class="fa-regular fa-user"></i> */}
+              <i
+                className='fa-solid fa-bars text-white'
+                style={{ cursor: 'pointer' }}
+              ></i>
             </div>
+            <a href='#'>
+              <img
+                src='/rguktLogo.png'
+                style={{
+                  width: '60px',
+                  height: '60px'
+                }}
+                className='rotating-image'
+              ></img>
+            </a>
+
+            <h1 style={{ color: 'rgb(137, 24, 26)' }}>Stay Master</h1>
+          </div>
+          {isDivVisible && (
+            <div
+              style={{
+                border: '1px solid white',
+                position: 'fixed',
+                // bottom: "0",
+                top: '5rem',
+                left: '0',
+                width: '100%',
+                height: '8rem',
+                backgroundColor: 'white', // Black background
+                zIndex: 2000 // Higher than other elements
+              }}
+            ></div>
           )}
+          {isLoggedIn ? (
+            <div className='header-links'>
+              <div className='first'>
+                {typeUser ? (
+                  <ul>
+                    <li>
+                      <a
+                        href='/caretaker/home'
+                        onClick={() => handleLinkClick('home')}
+                        className={activeLink === 'home' ? 'active-link' : ''}
+                      >
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/caretaker/outpasses'
+                        onClick={() => handleLinkClick('request-outpass')}
+                        className={
+                          activeLink === 'request-outpass' ? 'active-link' : ''
+                        }
+                      >
+                        Outpasses
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/caretaker/issues'
+                        onClick={() => handleLinkClick('raise-complaint')}
+                        className={
+                          activeLink === 'raise-complaint' ? 'active-link' : ''
+                        }
+                      >
+                        Issues
+                      </a>
+                    </li>
+                  </ul>
+                ) : (
+                  <ul>
+                    <li>
+                      <a
+                        href='/student/home'
+                        onClick={() => handleLinkClick('home')}
+                        className={activeLink === 'home' ? 'active-link' : ''}
+                      >
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/student/request'
+                        onClick={() => handleLinkClick('request-outpass')}
+                        className={
+                          activeLink === 'request-outpass' ? 'active-link' : ''
+                        }
+                      >
+                        Request a Outpass
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/student/issue'
+                        onClick={() => handleLinkClick('raise-complaint')}
+                        className={
+                          activeLink === 'raise-complaint' ? 'active-link' : ''
+                        }
+                      >
+                        Raise a Complaint
+                      </a>
+                    </li>
+                  </ul>
+                )}
+              </div>
+
+              <div className='profile' onClick={handleDivClick}>
+                <img
+                  src='/virat kohli.jpg'
+                  alt='Profile Icon'
+                  style={{
+                    height: '1.8rem',
+                    cursor: 'pointer',
+                    height: '36px',
+                    width: '36px',
+                    borderRadius: '50%'
+                  }}
+                />
+              </div>
+              {isBoxVisible && (
+                <div
+                  ref={popupRef} // Attach the reference to the popup
+                  style={{
+                    position: 'fixed',
+                    top: '80px',
+                    right: '30px',
+                    padding: '2rem',
+                    borderRadius: '20px',
+                    color: 'white',
+                    width: '12.5rem',
+                    height: '17rem',
+                    backgroundColor: '#3B3B3B',
+                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+                    textAlign: 'center',
+                    zIndex: 1000 // Ensures the dialog is above other elements
+                  }}
+                >
+                  <img
+                    src='/virat kohli.jpg'
+                    alt='Profile Icon'
+                    style={{
+                      height: '1.8rem',
+                      cursor: 'pointer',
+                      height: '72px',
+                      width: '72px',
+                      borderRadius: '50%',
+                      marginLeft: '2rem',
+                      marginBottom: '1rem'
+                    }}
+                  />
+
+                  <h1 style={{ fontSize: '1.8rem' }}> Hi Shaik !</h1>
+                  <p style={{ fontSize: '15px' }}>rr200088@rguktrkv.ac.in</p>
+                  <br></br>
+                  <p>Profile Details</p>
+                  <Link to='/'>
+                    <button
+                      onClick={handleLogout}
+                      style={{
+                        backgroundColor: 'rgb(137, 24, 26)',
+                        color: 'white',
+                        border: 'none',
+                        padding: '5px 10px',
+                        borderRadius: '5px',
+                        cursor: 'pointer',
+                        width: '100%'
+                      }}
+                    >
+                      Logout
+                    </button>
+                  </Link>
+                </div>
+              )}
+            </div>
+          ):(
+            <ul>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/signup">Signup</Link></li>
+            </ul>
+          )}
+          
         </div>
-        {/* </div> */}
-      </div>
       </div>
     </>
   )
