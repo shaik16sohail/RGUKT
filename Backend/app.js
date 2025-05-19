90cfe4a2fee714d0af5a0f17d76173a747c77dfb
shaik sohail <shaik16sohail@gmail.com>
2025-05-19 12:17:42 +0530

    Refactor DB connection to config/db.js for better structure

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express=require('express');
-const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app=express();
@@ -8,6 +7,7 @@ const authRoutes = require("./src/routes/authRoutes");
 const studentRoutes=require('./src/routes/studentRoutes');
 const port=process.env.PORT|| 8080;
 const cookieParser = require("cookie-parser");
+const connectDB = require("./src/config/db");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser());
@@ -15,17 +15,7 @@ app.use(cors({
   origin: "http://localhost:5173", // React frontend URL
   credentials: true // Allow cookies to be sent
 }));
-async function main() {
-  await mongoose.connect(process.env.MONGO_URL);
-}
-
-
-
-main()
-    .then(() => {
-         console.log("Connected to MongoDB Atlas");
-     })
-    .catch((err) => console.log(err));
+connectDB();
 
 app.use("/api/auth",authRoutes);
 app.use("/student",studentRoutes);
