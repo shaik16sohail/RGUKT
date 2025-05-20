e871a2aefd2f15326a7e07b1e07a8a8400888ea6
shaik sohail <shaik16sohail@gmail.com>
2025-05-20 21:39:07 +0530

    fix:Updated the model of Issue

--- Backend/src/models/Issue.js
+++ Backend/src/models/Issue.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 
 const issueSchema = new mongoose.Schema({
   studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
-  category: { type: String, required: true }, // Example: "Electricity", "Plumbing", "WiFi"
+  category: { 
+    type: String, 
+    enum: ["Electricity","Sanitation","Drinking_Water","Others"], 
+    required: true 
+  },
   description: { type: String, required: true },
   photo: { type: String }, // Store image URL if uploaded
-  status: { type: String, enum: ["pending", "in-progress", "resolved"], default: "pending" },
+  status: { 
+    type: String, 
+    enum: ["pending", "in-progress", "resolved"], 
+    default: "pending" 
+  },
+  hostelName: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
