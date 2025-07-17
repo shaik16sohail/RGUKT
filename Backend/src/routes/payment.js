0661d4dbdefa1e7ec2436769c3eeff50b5701e65
shaik sohail <shaik16sohail@gmail.com>
2025-07-17 16:04:18 +0530

    Updated Payment route

--- Backend/src/routes/payment.js
+++ Backend/src/routes/payment.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Stripe = require('stripe');
+const tempFormData = require('../utils/tempFormData');
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-router.post('/create-checkout-session', async (req, res) => {
-  const { amount } = req.body;
 
+
+router.post('/create-checkout-session', async (req, res) => {
+  const { formData,userId } = req.body;
+  console.log("in session checkout",userId);
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
@@ -14,18 +17,22 @@ router.post('/create-checkout-session', async (req, res) => {
           price_data: {
             currency: 'usd',
             product_data: {
-              name: 'Sample Payment',
+              name: 'Emergency Outpass Fee',
             },
-            unit_amount: amount * 100, // in cents
+            unit_amount: 500, // in cents
           },
           quantity: 1,
         },
       ],
       mode: 'payment',
-      success_url: 'http://localhost:3000/success',
-      cancel_url: 'http://localhost:3000/cancel',
+      success_url: 'http://localhost:5174/success',
+      cancel_url: 'http://localhost:5174/cancel',
+      metadata: {
+        userId,
+        studentMobile: formData.studentMobile,
+      },
     });
-
+    tempFormData[session.id]=formData;
     res.json({ id: session.id });
   } catch (err) {
     res.status(500).json({ error: err.message });
